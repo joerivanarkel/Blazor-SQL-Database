5436b8381ff98a20966418f282c94a64bea8989e
joerivanarkel <Joeri@vanarkel.net>
2022-03-02 14:42:08 +0100

    Moved COnnection String to json file

--- Data/Database.cs
+++ Data/Database.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography.X509Certificates;
 using Common.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Data
 {
@@ -12,9 +13,19 @@ namespace Data
         public DbSet<Occupation> Occupations { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Region> Regions { get; set; }
+
+        DatabaseConfig _config = GetConfig();
+        static DatabaseConfig GetConfig()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("connectionstring.json")
+                .AddEnvironmentVariables()
+                .Build();
+            return config.GetSection("myConfig").Get<DatabaseConfig>();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=192.168.2.10\\SQLEXPRESS;Database=21-2;user id=sa;password=123;");
+            optionsBuilder.UseSqlServer(_config.ConnectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Data/DatabaseConfig.cs
@@ -0,0 +1,7 @@
+namespace Data
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
